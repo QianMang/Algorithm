e8a8405e859df282f6aecaa0ea25869cdb86cf34
mangAI <d2385914340@gmail.com>
2019-09-01 10:15:57 -0400

    mheap

---
+++ heap/topK_frequency.cpp
@@ -0,0 +1,34 @@
+//leetcode p357
+//how to use priority_queue for topK problem
+//especially cmp
+class Solution {
+public:
+    struct cmp{
+        bool operator()(pair<int,int> a, pair<int,int> b){
+            return a.second > b.second;
+        }
+    };
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        vector<int> result;
+        unordered_map<int,int > frequency;
+        priority_queue<pair<int,int>, vector<pair<int,int>> , cmp> topK;
+        for(auto it = nums.cbegin(); it != nums.cend(); ++it){
+            if(frequency[*it] == NULL){
+                frequency[*it] = 1;
+            }
+             ++frequency[*it];
+        }
+        for(auto it = frequency.cbegin(); it != frequency.cend();++it){
+            topK.push(*it);
+            if(topK.size() > k){
+                topK.pop();
+            }
+            
+        }
+        while(!topK.empty()){
+            result.push_back(topK.top().first);
+            topK.pop();
+        }
+        return result;
+    }
+};
