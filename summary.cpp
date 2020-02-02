3029593911b2ab1d9b229658f5bfcb3bcf457de0
mangAI <d2385914340@gmail.com>
2020-02-01 20:45:45 -0500

    LRU

--- summary.cpp
+++ summary.cpp
@@ -1,37 +1,37 @@
 //quick sort
 
 int partition(vector<int>& nums, int left, int right){
-        if(left >= right){
-            return left;
+    if(left >= right){
+        return left;
+    }
+    
+    int key = nums[(left+right)/2];
+    swap(nums[left],nums[(left+right)/2]);
+    int index_left = left;
+    int index_right = right;
+    while(index_left < index_right){
+        while(index_left < index_right && nums[index_right] >= key){   //从小到大 和 从大到小
+            --index_right;
         }
-        
-        int key = nums[(left+right)/2];
-        swap(nums[left],nums[(left+right)/2]);
-        int index_left = left;
-        int index_right = right;
-        while(index_left < index_right){
-            while(index_left < index_right && nums[index_right] >= key){   //从小到大 和 从大到小
-                --index_right;
-            }
-            while(index_left < index_right && nums[index_left] <= key){
-                ++index_left;
-            }
-            if(index_left<index_right)
-                swap(nums[index_right], nums[index_left]);
-            
+        while(index_left < index_right && nums[index_left] <= key){
+            ++index_left;
         }
-        swap(nums[index_left],nums[left]);
-        //变体
-        if(index_left == nums.size()/2 ){
-            return index_left;
-        }
-        else if(index_left < nums.size()/2){
-            return partition(nums,index_left+1,right);
-        }else{
-            return partition(nums,left,index_left - 1);
-        }
+        if(index_left<index_right)
+            swap(nums[index_right], nums[index_left]);
         
     }
+    swap(nums[index_left],nums[left]);
+    //变体
+    if(index_left == nums.size()/2 ){
+        return index_left;
+    }
+    else if(index_left < nums.size()/2){
+        return partition(nums,index_left+1,right);
+    }else{
+        return partition(nums,left,index_left - 1);
+    }
+    
+}
 
 
 
@@ -68,3 +68,50 @@ void mergeSort(vector<int>& nums,int left,int right){
             nums[i] = temp[i-left];
         }
     }
+
+
+
+//LRU
+class LRUCache {
+public:
+    LRUCache(int capacity) {
+       _capacity=capacity;
+        
+    }
+    
+    int get(int key) {
+        if(Cache.find(key) == Cache.end()){
+            return -1;
+        }
+        rank.erase(index[key]);
+        rank.push_front(key);
+        index[key] = rank.begin();
+        return Cache[key];
+    }
+    
+    void put(int key, int value) {
+       if(Cache.find(key) == Cache.end()){
+            if(Cache.size() == _capacity){
+                int pop_one = rank.back();
+                rank.pop_back();
+                Cache.erase(pop_one);
+                index.erase(pop_one);
+            }
+            Cache[key] = value;
+            rank.push_front(key);
+            index[key] = rank.begin();
+       }else{
+            Cache[key] = value;
+            rank.erase(index[key]);
+            rank.push_front(key);
+            index[key] = rank.begin();
+       }
+        
+    }
+    
+private:
+    unordered_map<int,int> Cache;
+    unordered_map<int, list<int>::iterator> index;
+    list<int> rank;
+    int _capacity;
+};
